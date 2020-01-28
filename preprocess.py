78c64a2b9d323a63c607d7455e46c38a3daf7021
liujjpp <liujjpp@gmail.com>
2020-01-27 23:49:00 -0600

    Filters can receive a year as parameter

--- preprocess.py
+++ preprocess.py
@@ -2,42 +2,99 @@ import json
 import nltk
 from nltk.tokenize import RegexpTokenizer
 
-def best_filter(src_path='./gg2020.json', dest_path='./gg2020_best.json'):
+def best_filter(year):
+    src_path = './gg' + str(year) + '.json'
+    dest_path = './gg' + str(year) + '_best.json'
+
     with open(src_path, 'r') as fin:
         with open(dest_path, 'w') as fout:
             for tweet in fin.readlines():
                 tweet = json.loads(tweet)
+                if isinstance(tweet, list):
+                    for t in tweet:
+                        if 'best' in tweet['text'] or 'Best' in tweet['text'] or 'BEST' in tweet['text']:
+                            new_tweet = {}
+                            new_tweet['text'] = t['text']
+                            json.dump(new_tweet, fout)
+                            fout.write('\n')
+                    continue
                 if 'best' in tweet['text'] or 'Best' in tweet['text'] or 'BEST' in tweet['text']:
                     new_tweet = {}
                     new_tweet['text'] = tweet['text']
-                    new_tweet['created_at'] = tweet['created_at']
-                    new_tweet['user'] = tweet['user']
+                    # new_tweet['created_at'] = tweet['created_at']
+                    # new_tweet['user'] = tweet['user']
                     json.dump(new_tweet, fout)
                     fout.write('\n')
 
-def host_filter(src_path='./gg2020.json', dest_path='./gg2020_host.json'):
+def award_filter(year):
+    src_path = './gg' + str(year) + '.json'
+    dest_path = './gg' + str(year) + '_award.json'
+
     with open(src_path, 'r') as fin:
         with open(dest_path, 'w') as fout:
             for tweet in fin.readlines():
                 tweet = json.loads(tweet)
-                if 'host' in tweet['text'] or 'Host' in tweet['text'] or 'HOST' in tweet['text']:
+                if isinstance(tweet, list):
+                    for t in tweet:
+                        if 'award' in tweet['text'] or 'Award' in tweet['text']:
+                            new_tweet = {}
+                            new_tweet['text'] = t['text']
+                            json.dump(new_tweet, fout)
+                            fout.write('\n')
+                    continue
+                if 'award' in tweet['text'] or 'Award' in tweet['text']:
                     new_tweet = {}
                     new_tweet['text'] = tweet['text']
-                    new_tweet['created_at'] = tweet['created_at']
-                    new_tweet['user'] = tweet['user']
+                    # new_tweet['created_at'] = tweet['created_at']
+                    # new_tweet['user'] = tweet['user']
                     json.dump(new_tweet, fout)
                     fout.write('\n')
 
-def dress_filter(src_path='./gg2020.json', dest_path='./gg2020_dress.json'):
+def host_filter(year):
+    src_path = './gg' + str(year) + '.json'
+    dest_path = './gg' + str(year) + '_host.json'
+
     with open(src_path, 'r') as fin:
         with open(dest_path, 'w') as fout:
             for tweet in fin.readlines():
                 tweet = json.loads(tweet)
+                if isinstance(tweet, list):
+                    for t in tweet:
+                        if ('host' in t['text'] or 'Host' in t['text']) and not ('next' in t['text'] or 'Next' in t['text']):
+                            new_tweet = {}
+                            new_tweet['text'] = t['text']
+                            json.dump(new_tweet, fout)
+                            fout.write('\n')
+                    continue
+                if ('host' in t['text'] or 'Host' in t['text']) and not ('next' in t['text'] or 'Next' in t['text']):
+                    new_tweet = {}
+                    new_tweet['text'] = tweet['text']
+                    # new_tweet['created_at'] = tweet['created_at']
+                    # new_tweet['user'] = tweet['user']
+                    json.dump(new_tweet, fout)
+                    fout.write('\n')
+
+def dress_filter(year):
+    src_path = './gg' + str(year) + '.json'
+    dest_path = './gg' + str(year) + '_dress.json'
+
+    with open(src_path, 'r') as fin:
+        with open(dest_path, 'w') as fout:
+            for tweet in fin.readlines():
+                tweet = json.loads(tweet)
+                if isinstance(tweet, list):
+                    for t in tweet:
+                        if 'dress' in tweet['text'] or 'Dress' in tweet['text'] or 'DRESS' in tweet['text']:
+                            new_tweet = {}
+                            new_tweet['text'] = t['text']
+                            json.dump(new_tweet, fout)
+                            fout.write('\n')
+                    continue
                 if 'dress' in tweet['text'] or 'Dress' in tweet['text'] or 'DRESS' in tweet['text']:
                     new_tweet = {}
                     new_tweet['text'] = tweet['text']
-                    new_tweet['created_at'] = tweet['created_at']
-                    new_tweet['user'] = tweet['user']
+                    # new_tweet['created_at'] = tweet['created_at']
+                    # new_tweet['user'] = tweet['user']
                     json.dump(new_tweet, fout)
                     fout.write('\n')
 
@@ -53,6 +110,7 @@ def tweets_to_words(src_path):
 
 
 if __name__ == "__main__":
-    best_filter()
-    host_filter()
-    dress_filter()
+    # best_filter()
+    # award_filter()
+    # host_filter()
+    # dress_filter()
